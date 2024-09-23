37e853415bdadf3a8e5938d0abd89233b76b7136
vemulavenky <vemulavenky08@gmail.com>
2024-09-23 14:24:04 +0530

    Add files via upload

---
+++ cost_center.sql
@@ -0,0 +1,9 @@
+INSERT INTO cost_center (cost_center_code, cost_center) VALUES ('000', 'Policies');
+INSERT INTO cost_center (cost_center_code, cost_center) VALUES ('001', 'Product Engineering Services (PES)');
+INSERT INTO cost_center (cost_center_code, cost_center) VALUES ('002', 'Product Engineering (PE)');
+INSERT INTO cost_center (cost_center_code, cost_center) VALUES ('003', 'Industrially Automation Services (IAS)');
+INSERT INTO cost_center (cost_center_code, cost_center) VALUES ('004', 'Industrially Automation (IA)');
+INSERT INTO cost_center (cost_center_code, cost_center) VALUES ('005', 'Digital Business (DB)');
+INSERT INTO cost_center (cost_center_code, cost_center) VALUES ('006', 'Biometrics Technology (BT)');
+INSERT INTO cost_center (cost_center_code, cost_center) VALUES ('007', 'Support System');
+INSERT INTO cost_center (cost_center_code, cost_center) VALUES ('008', 'Process Quality Assurance (PQA)');
---
+++ document_type.sql
@@ -0,0 +1,9 @@
+INSERT INTO document_type (document_type_code, document_type) VALUES
+('01', 'Policy'),
+('02', 'Pre-sales'),
+('03', 'Process Quality Assurance'),
+('04', 'Process Management'),
+('05', 'Process Asset Development'),
+('06', 'Requirement Development & Management'),
+('07', 'Decision Analysis & Resolution'),
+('08', 'Estimating');
